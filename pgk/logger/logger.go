916f2db327f62335fbe82771b379f6612f1a318c
Teixeira Fialho <pedrofilipefialho19@gmail.com>
2026-02-10 23:05:30 +0000

    logger struct

--- pgk/logger/logger.go
+++ pgk/logger/logger.go
@@ -1,3 +1,18 @@
 package logger
 
-//todo: create custom logger that puts into the file
+// todo: create custom logger that puts into the file
+var debugFilename = "debug-ripple"
+
+type Logger struct {
+	debugMode bool
+}
+
+func Init(debugMode bool) *Logger {
+	return &Logger{
+		debugMode: debugMode,
+	}
+}
+
+func (l *Logger) Println(str string) {
+
+}
